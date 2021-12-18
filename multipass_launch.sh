5ce0d5526241710132895e0fb30b2beb28fe72ad
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2021-12-18 23:06:06 +0200

    Add node-playbook to K8s with MetalLB

--- multipass_launch.sh
+++ multipass_launch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 multipass launch --name master -c 2 -m 2G --cloud-init ./cloud-init.yml &> /dev/null &
-for i in {1..3}
+echo 'Launched master!'
+for i in {1..5}
 do
  multipass launch --name node$i -c 2 -m 2G --cloud-init ./cloud-init.yml &> /dev/null &
+ echo "Lanched node$i"
 done
